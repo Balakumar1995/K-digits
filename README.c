012e81c5e07af16e1e361c74574dc6e371012f46
Balakumar1995 <sbalakumarece@gmail.com>
2016-06-07 10:29:42 +0530

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+long long int cumfun(const void * a, const void * b)
+{
+   return ( *(long long int*)a - *(long long int*)b );
+}
+
+void main()
+{
+long long int arr[1000],number,i,k,l;
+char str[10000];
+scanf("%s",str);
+scanf("%lld",&k);
+l=strlen(str);
+for(i=0;i<l;i++)
+	arr[i]=(int)str[i]%48;
+qsort(arr,l,sizeof(long long int), cumfun);
+for(i=0;i<l-k;i++)
+printf("%lld",arr[i]);
+}
